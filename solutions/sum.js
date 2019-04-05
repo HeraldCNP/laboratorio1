205c5e4f473e72ce3bc23b8d1b70a6a64b56993b
Herald <heraldcnp@gmail.com>
2019-04-04 20:53:07 -0400

    arreglado hacer pull

--- solutions/sum.js
+++ solutions/sum.js
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-function sum(a,b) {
-  return a+b;
-}
-module.exports=sum
-=======
 function suma(a, b) {
     return a + b;
 }
 module.exports = suma
->>>>>>> 1f4b8806573226a31d60fe7b29acdfe6ab645aa9
